2fbdbf127a1c202d879d5564474b08132a8e5eca
AHDevZone <167384580+AHDevZone@users.noreply.github.com>
2024-04-23 01:12:40 +0200

    Add files via upload

---
+++ cube.js
@@ -0,0 +1,42 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+var canvas = document.getElementById("cube")
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize( canvas.width, canvas.height );
+renderer.setClearColor(0xffffff, 0);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 45, canvas.width / canvas.height, 1, 10000 );
+const controls = new OrbitControls( camera, renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+
+const material = [
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_right )} ), // droite
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_left )} ), // gauche
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_up )} ), // haut
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_down )} ), // bas
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_front )} ), // avant
+  new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load( face_back )} ), // arriere
+];
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.y = 2;
+camera.position.z = 4.5;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.update();
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	controls.update();
+  
+	renderer.render( scene, camera );
+}
+
+animate();
